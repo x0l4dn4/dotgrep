59fb150693655beb27641636eaa5e2fc0b28747f
x0l4dn4 <danielcallejo.dev@gmail.com>
2025-10-14 17:58:44 +0200

    intput from terminal

--- dotgrep/Program.cs
+++ dotgrep/Program.cs
@@ -4,9 +4,16 @@
     {
         static void Main(string[] args)
         {
+            // TODO: read input from a file
             string input = Console.In.ReadToEnd();
             Console.WriteLine(input);
-            Environment.Exit(0);
+            if (input.Contains(args[1]))
+            {
+                Console.WriteLine(input);
+                Environment.Exit(0);
+            }
+            Environment.Exit(1);
+
         }
     }
 }
