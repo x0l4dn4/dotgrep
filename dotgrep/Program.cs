853425e629ddac6cbfad714f7e0d371d592b7ce8
x0l4dn4 <danielcallejo.dev@gmail.com>
2025-10-14 15:16:57 +0200

    first commit

---
+++ dotgrep/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace dotgrep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.In.ReadToEnd();
+            Console.WriteLine(input);
+            Environment.Exit(0);
+        }
+    }
+}
